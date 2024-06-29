aa4848f9af3254ccc9dae5aa50830eccc495815c
bhargav <bhargav2520@gmail.com>
2024-06-29 10:32:18 -0400

    first commit

---
+++ companyms/src/main/java/com/embarkx/companyms/CompanymsApplication.java
@@ -0,0 +1,17 @@
+package com.embarkx.companyms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+
+
+public class CompanymsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanymsApplication.class, args);
+	}
+
+}
